242a73b2760dd965b1360591143dd562f50ceb0c
Cha1mae <chaimastaifa@gmail.com>
2024-04-30 14:06:09 -0700

    cleen up

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -19,28 +19,16 @@ def canUnlockAll(boxes):
     if n == 0:
         return True
 
-    seen = set()  # Set to keep track of visited boxes
-    Qyu = deque([0])  # Start with the first box (index 0)
+    seen = set()
+    Qyu = deque([0])
 
     while Qyu:
-        thee_box = Qyu.popleft()  # Dequeue the next box to explore
+        thee_box = Qyu.popleft()
         if thee_box not in seen:
-            seen.add(thee_box)  # Mark the box as visited
+            seen.add(thee_box)
             keys = boxes[thee_box]
             for key in keys:
                 if key < n and key not in seen:
-                    Qyu.append(key)  # Enqueue the key's box if not visited
+                    Qyu.append(key)
 
     return len(seen) == n
-
-# Test cases
-
-
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # Output: True
-
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # Output: True
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))  # Output: False
