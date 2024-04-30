089f54c66e9b11e6b22cd64ea1c8d692351fad4a
Cha1mae <chaimastaifa@gmail.com>
2024-04-30 13:52:31 -0700

    added test cases

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,13 +2,12 @@
 
 from collections import deque
 
-
 def canUnlockAll(boxes):
     if not boxes:
         return False
 
-    g = len(boxes)
-    if g == 0:
+    n = len(boxes)
+    if n == 0:
         return True
 
     seen = set()
@@ -18,9 +17,19 @@ def canUnlockAll(boxes):
         thee_box = Qyu.popleft()
         if thee_box not in seen:
             seen.add(thee_box)
-            keys = boxes[thee_box] 
+            keys = boxes[thee_box]
             for key in keys:
-                if key < g and key not in seen:
+                if key < n and key not in seen:
                     Qyu.append(key)
 
-    return len(seen) == g
+    return len(seen) == n
+
+# Test cases
+boxes1 = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes1))  # Output: True
+
+boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes2))  # Output: True
+
+boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes3))  # Output: False
