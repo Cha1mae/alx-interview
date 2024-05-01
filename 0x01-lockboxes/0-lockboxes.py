c2b5b814c83370bff025a0398d83fa2172b20e00
Cha1mae <chaimastaifa@gmail.com>
2024-05-01 12:58:44 -0700

    testing a new function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,45 +1,17 @@
 #!/usr/bin/python3
-from collections import deque
-
 
 def canUnlockAll(boxes):
-    """ Determine if all boxes can be unlocked.
-
-    Args:
-        boxes (list of list of int): A list of boxes where
-        each box contains keys to other boxes.
-
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
-    if not boxes:
-        return False
-
+    """Determine if all boxes can be opened."""
     n = len(boxes)
-    if n == 0:
-        return True
+    keys = [0]
+    opened = [False] * n
+    opened[0] = True
 
-    seen = set()
-    Qyu = deque([0])
+    while keys:
+        key = keys.pop()
+        for box in boxes[key]:
+            if box < n and not opened[box]:
+                keys.append(box)
+                opened[box] = True
 
-    while Qyu:
-        thee_box = Qyu.popleft()
-        if thee_box not in seen:
-            seen.add(thee_box)
-            keys = boxes[thee_box]
-            for key in keys:
-                if key < n and key not in seen:
-                    Qyu.append(key)
-
-    return len(seen) == n
-# Test cases
-
-
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # Output: True
-
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # Output: True
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))  # Output: False
+    return all(opened) 
