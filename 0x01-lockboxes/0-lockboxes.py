fc5c88b78a1809b454b82ccc3bb0718099ecc425
Cha1mae <chaimastaifa@gmail.com>
2024-05-02 10:13:35 -0700

    check

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 
-"""0-lockboxes.py"""
+"""Module for unlocking boxes"""
 
 
 def canUnlockAll(boxes):
-    """method that determines if all the boxes can be opened"""
+    """
+    Function to verify if every single box can be unlocked.
+    boxes : list of lists where each sublist
+    represents a box and contains keys.
+    """
     n = len(boxes)
-    unlocked = set([0])
-    keys = boxes[0]
-    stack = list(keys)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = [key for key in boxes[0] if key < n]
 
-    while stack:
-        key = stack.pop()
-        if key < n and key not in unlocked:
-            unlocked.add(key)
-            stack.extend(boxes[key])
+    while keys:
+        new_keys = []
+        for key in keys:
+            if not unlocked[key]:
+                unlocked[key] = True
+                for k in boxes[key]:
+                    if k < n and not unlocked[k]:
+                        new_keys.append(k)
+        keys = new_keys
 
-    return len(unlocked) == n
+    return all(unlocked)
