be253d1253f7afcb9bdd4599c3c6318d1ece4271
Cha1mae <chaimastaifa@gmail.com>
2024-05-08 13:59:55 -0700

    :)

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,10 +6,10 @@ def minOperations(n):
     if n <= 1:
         return 0
     n_o = 0
-    p = 2
+    d = 2
     while n > 1:
-        while n % p == 0:
-            n_o += p
-            n //= p
-        p += 1
+        while n % d == 0:
+            n_o += d
+            n //= d
+        d += 1
     return n_o
