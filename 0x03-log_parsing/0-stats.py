1e6c986ac8be56a76c096bec19b7d84e000da979
Cha1mae <chaimastaifa@gmail.com>
2024-05-16 02:09:44 -0700

    lets try

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,10 +2,9 @@
 import sys
 import signal
 
-
 """Initializing the variables"""
-total_size = 0
-status_codes = {
+in_total = 0
+code_st = {
     "200": 0,
     "301": 0,
     "400": 0,
@@ -18,19 +17,19 @@ status_codes = {
 
 
 def print_stats():
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+    print("File size: {}".format(in_total))
+    for code in sorted(code_st.keys()):
+        if code_st[code] > 0:
+            print("{}: {}".format(code, code_st[code]))
 
 
-def signal_handler(sig, frame):
+def handler_s(sig, frame):
     print_stats()
     sys.exit(0)
 
 
 """Handling the signal"""
-signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGINT, handler_s)
 
 try:
     for i, line in enumerate(sys.stdin, 1):
@@ -38,9 +37,9 @@ try:
             data = line.split()
             size = data[-1]
             status_code = data[-2]
-            total_size += int(size)
-            if status_code in status_codes:
-                status_codes[status_code] += 1
+            in_total += int(size)
+            if status_code in code_st:
+                code_st[status_code] += 1
         except Exception as e:
             pass
         if i % 10 == 0:
